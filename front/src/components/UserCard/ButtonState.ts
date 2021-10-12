765d1281c47beb6e10d284bc7e477c3e26debebf
yuto-moriizumi <moriizumi@design.inf.shizuoka.ac.jp>
2021-10-12 21:32:51 +0900

    不要なstateクラスを削除

--- front/src/components/UserCard/ButtonState.ts
+++
@@ -1,9 +0,0 @@
-import { ButtonVariant } from 'react-bootstrap/esm/types';
-
-export default abstract class ButtonState {
-  abstract display: JSX.Element;
-
-  abstract variant: ButtonVariant;
-
-  abstract disabled: boolean;
-}
